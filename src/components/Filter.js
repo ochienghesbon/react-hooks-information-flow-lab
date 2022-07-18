24e384e828ea05ccd06896ffbaac7f59731213e6
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-19 00:48:19 +0300

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,13 @@
 import React from "react";
 
-function Filter({onCategoryChange}){
-
-return(
-    <div className="Filter">
-        <select name="filter" onChange={onCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div> 
-      
-      )
-}
-
-export default Filter
+const Filter = ({ onCategoryChange }) => {
+  return (
+    <select name="filter" onChange={onCategoryChange}>
+      <option value="All">Filter by category</option>
+      <option value="Produce">Produce</option>
+      <option value="Dairy">Dairy</option>
+      <option value="Dessert">Dessert</option>
+    </select>
+  );
+};
+export default Filter;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-
-function Header({onDarkModeClick,text}) {
-    return(
-        <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {text ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-    )
-}
-
-export default Header
+const Header = ({ onDarkModeClick, isDarkMode }) => {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+};
+export default Header;
